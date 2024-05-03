83e89ff03cb4535a2a1a476877ebf927289d3801
Kenzie Lippa <68268031+KenzieLippa@users.noreply.github.com>
2024-05-02 21:21:30 -0400

    adding more chat functionality, looks dope ;P
    jk its all html rn oof

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,36 +1,40 @@
 import React from 'react'
 import './Chat.css'
 import aida from '../assets/HippieLogo.png'
-
+import addBtn from '../assets/add-30.png'
+import msgIcon from '../assets/message.svg'
+import home from '../assets/home.svg'
 const Chat = () => {
   return (
     <div className='GPT'>
       <div className="sidebar">
         <div className="upper-side">
-          <div className="upper-side-image"><img src={aida} alt="" /> <span className='Brand'>AIDA</span></div>
+          <div className="upper-side-image"><img src={aida} alt="Aida" className='logo'/> <span className='brand'>AIDA</span></div>
            
             <button className="newChat">
-              <img src="" alt="" className="add" />New Chat
+              <img src={addBtn} alt="" className="add" />New Chat
             </button>
             <div className="chat-logs">
               <button className="chat">
-                <img src="" alt="" className="chat-image" /> What is the meaning of life?
+                <img src={msgIcon} alt="chat" className="chat-image" /> What is the meaning of life?
               </button>
               <button className="chat">
-                <img src="" alt="" className="chat-image" /> What is the meaning of life?
+                <img src={msgIcon} alt="chat" className="chat-image" />Finishing hw and studying
               </button>
             
-          </div>
-        </div>
+             </div>
+         </div>
         <div className="lower-side">
-
+          <div className="list-items"><img src="" alt="" />Home</div>
+          <div className="list-items"><img src="" alt="" />Saved</div>
+          {/* could add another lower down if i want ect... */}
         </div>
       </div>
       <div className="main">
 
       </div>
       
-      </div>
+    </div>
   )
 }
 

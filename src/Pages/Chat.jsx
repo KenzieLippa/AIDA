8c64b180d594fc0c475cea027a4c4460b77b8b05
Kenzie Lippa <68268031+KenzieLippa@users.noreply.github.com>
2024-05-02 22:08:23 -0400

    fixing some absolute gorgeous html lol
    terrible looking before but now its all bouncy and fun!

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -4,6 +4,8 @@ import aida from '../assets/HippieLogo.png'
 import addBtn from '../assets/add-30.png'
 import msgIcon from '../assets/message.svg'
 import home from '../assets/home.svg'
+import saved from '../assets/bookmark.svg'
+
 const Chat = () => {
   return (
     <div className='GPT'>
@@ -12,21 +14,21 @@ const Chat = () => {
           <div className="upper-side-image"><img src={aida} alt="Aida" className='logo'/> <span className='brand'>AIDA</span></div>
            
             <button className="newChat">
-              <img src={addBtn} alt="" className="add" />New Chat
+              <img src={addBtn} alt="" className="addBtn" />New Chat
             </button>
             <div className="chat-logs">
               <button className="chat">
-                <img src={msgIcon} alt="chat" className="chat-image" /> What is the meaning of life?
+                <img src={msgIcon} alt="chat" className="chat-image" /> <p className="chat-title"> What is the meaning of life?</p>
               </button>
               <button className="chat">
-                <img src={msgIcon} alt="chat" className="chat-image" />Finishing hw and studying
+                <img src={msgIcon} alt="chat" className="chat-image" /><p className="chat-title">Finishing hw and studying</p>
               </button>
             
              </div>
          </div>
         <div className="lower-side">
-          <div className="list-items"><img src="" alt="" />Home</div>
-          <div className="list-items"><img src="" alt="" />Saved</div>
+          <div className="list-items"><img src={home} alt="" />Home</div>
+          <div className="list-items"><img src={saved} alt="" />Saved</div>
           {/* could add another lower down if i want ect... */}
         </div>
       </div>

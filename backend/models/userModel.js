ef3c44007d671ac1ad44595558cd394216eb10b0
Kenzie Lippa <68268031+KenzieLippa@users.noreply.github.com>
2024-05-04 23:53:04 -0400

    fixing stuff?

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,15 @@
 //define the model
 import mongoose from "mongoose"
+// const messagesSchema = new mongoose.Schema({
+//     content:{type: String, required:true},
+//     isBot:{type:Boolean, default:false}
+// });
 
 const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
+    // messages:[messagesSchema]
     //save anything else here like chat logs ect
 }, {minimize:false})
 
